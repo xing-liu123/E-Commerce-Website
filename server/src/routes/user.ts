66a37a639a97ad8b555c3f2d41f231d18d04f33c
Xavier Liu <u1314373@utah.edu>
2023-12-22 00:12:43 -0500

    Set up client part

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,8 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { IUser, UserModel } from "../models/user";
 import { UserErrors } from "../errors";
+import { verify } from "crypto";
 
 const router = Router();
 
@@ -44,12 +45,30 @@ router.post("/login", async (req: Request, res: Response) => {
       return res.status(400).json({ type: UserErrors.WRONG_CREDENTIALS });
     }
 
-    const token = jwt.sign({id: user._id}, "secret")
-    res.json({token, userID: user._id})
-
+    const token = jwt.sign({ id: user._id }, "secret");
+    res.json({ token, userID: user._id });
   } catch (err) {
     res.status(500).json({ type: err });
   }
 });
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, "secret", (err) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      next();
+    });
+  }
+
+  return res.sendStatus(401);
+};
 
 export { router as userRouter };
